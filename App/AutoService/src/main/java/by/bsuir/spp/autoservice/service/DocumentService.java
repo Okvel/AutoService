98f6499d2356285e760f138539a2ad3420e72fdc
AJIucard <evgeniyrileev@gmail.com>
2016-05-11 19:21:58 +0300

    #Lab_6. Implemented saving PDF files.

--- App/AutoService/src/main/java/by/bsuir/spp/autoservice/service/DocumentService.java
+++ App/AutoService/src/main/java/by/bsuir/spp/autoservice/service/DocumentService.java
@@ -2,15 +2,25 @@ package by.bsuir.spp.autoservice.service;
 
 import by.bsuir.spp.autoservice.entity.DocumentFormat;
 import by.bsuir.spp.autoservice.entity.DocumentType;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 
 public class DocumentService {
+    private static final String FONT_FILE = "font/arial.ttf";
+    private static final String DATE_FORMAT = "dd.MM.yyyy";
+
     private static DocumentService instance = new DocumentService();
     private static CellStyle defaultStyle;
     private static CellStyle headerStyle;
@@ -113,5 +123,73 @@ public class DocumentService {
         } catch (IOException ex) {
             throw new ServiceException(ex);
         }
+    }
+
+    private void savePdf(String fileName) throws ServiceException {
+        File outFile = new File(fileName + DocumentFormat.PDF.getFormat());
+        File inputFile = new File(fileName + DocumentFormat.XLSX.getFormat());
+
+        try (
+                FileInputStream fileInputStream = new FileInputStream(inputFile);
+                XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+                FileOutputStream fos = new FileOutputStream(outFile)
+                ) {
+            BaseFont baseFont = BaseFont.createFont(FONT_FILE, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            Document pdfDocument = new Document();
+            PdfWriter.getInstance(pdfDocument, fos);
+            pdfDocument.open();
+            PdfPTable table = null;
+
+            PdfPCell tableCell;
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.iterator();
+                while (cellIterator.hasNext()) {
+                    if (table == null) {
+                        table = new PdfPTable(iteratorSize(cellIterator));
+                        cellIterator = row.iterator();
+                    }
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            tableCell = new PdfPCell( new Phrase(String.valueOf(cell.getBooleanCellValue())));
+                            table.addCell(tableCell);
+                        case Cell.CELL_TYPE_STRING:
+                            table.addCell(new Paragraph(cell.getStringCellValue(), font));
+                            break;
+                        case Cell.CELL_TYPE_NUMERIC:
+                            if (cell.getNumericCellValue() > 42000) {
+                                SimpleDateFormat sf = new SimpleDateFormat(DATE_FORMAT);
+                                String formattedDate = sf.format(cell.getDateCellValue());
+                                tableCell = new PdfPCell(new Phrase(formattedDate));
+                                table.addCell(tableCell);
+                            } else {
+                                table.addCell(String.valueOf(cell.getNumericCellValue()));
+                            }
+                            break;
+                        default:
+                            tableCell = new PdfPCell(new Phrase(" "));
+                            table.addCell(tableCell);
+                    }
+                }
+            }
+            pdfDocument.add(table);
+            pdfDocument.close();
+        } catch (IOException | DocumentException ex) {
+            throw new ServiceException(ex);
+        }
+    }
+
+    private int iteratorSize(Iterator iterator) {
+        int size = 0;
+        for (; iterator.hasNext();) {
+            iterator.next();
+            size++;
+        }
+
+        return size;
     }
 }
