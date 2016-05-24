ff6e836252a717f8163e55e0470d2e9fe551aa10
Okvel <okvel.s@gmail.com>
2016-05-24 18:14:28 +0300

    #Lab_7. Created show act action.

---
+++ App/src/main/java/by/bsuir/spp/autoservice/action/ShowActAction.java
@@ -0,0 +1,38 @@
+package by.bsuir.spp.autoservice.action;
+
+import by.bsuir.spp.autoservice.entity.Act;
+import by.bsuir.spp.autoservice.service.ActService;
+import by.bsuir.spp.autoservice.service.ServiceException;
+import com.opensymphony.xwork2.Action;
+import org.apache.log4j.Logger;
+
+public class ShowActAction implements Action {
+    private static Logger logger = Logger.getLogger(ShowActAction.class);
+
+    private long actId;
+    private Act act;
+
+    @Override
+    public String execute() throws Exception {
+        String result = ERROR;
+        ActService service = ActService.getInstance();
+        try {
+            act = service.findById(actId);
+            if (act != null) {
+                result = SUCCESS;
+            }
+        } catch (ServiceException ex) {
+            logger.error("Show act action error", ex);
+        }
+
+        return result;
+    }
+
+    public void setActId(long actId) {
+        this.actId = actId;
+    }
+
+    public Act getAct() {
+        return act;
+    }
+}
