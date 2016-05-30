1fb62016946473b43c8785c60b10e8c1de2704f4
AJIucard <evgeniyrileev@gmail.com>
2016-05-30 14:14:44 +0300

    #Lab_3. Minor bug fixes.

--- App/AutoService/src/main/java/by/bsuir/spp/autoservice/dao/impl/mysql/MySqlDetailDao.java
+++ App/AutoService/src/main/java/by/bsuir/spp/autoservice/dao/impl/mysql/MySqlDetailDao.java
@@ -4,6 +4,7 @@ import by.bsuir.spp.autoservice.dao.DaoException;
 import by.bsuir.spp.autoservice.dao.DetailDao;
 import by.bsuir.spp.autoservice.dao.util.DatabaseUtil;
 import by.bsuir.spp.autoservice.entity.Detail;
+import org.apache.poi.ss.formula.functions.Na;
 
 import javax.naming.NamingException;
 import java.sql.Connection;
@@ -14,7 +15,10 @@ import java.util.Collection;
 
 class MySqlDetailDao implements DetailDao {
     private static final String SQL_SELECT_BY_NAME = "SELECT id FROM detail WHERE name = ?";
+    private static final String SQL_SELECT_BY_ID = "SELECT id, name FROM detail WHERE id = ?";
     private static final String SQL_INSERT = "INSERT INTO detail(name) VALUE (?)";
+
+    private static final String COLUMN_NAME_NAME = "name";
 
     private static MySqlDetailDao instance = new MySqlDetailDao();
 
@@ -26,7 +30,20 @@ class MySqlDetailDao implements DetailDao {
 
     @Override
     public Detail findById(Long id) throws DaoException {
-        return null;
+        Detail detail = null;
+        try(
+                Connection connection = DatabaseUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(SQL_SELECT_BY_ID)
+                ){
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                detail = fillDetail(resultSet);
+            }
+        } catch (SQLException | NamingException ex){
+            throw new DaoException(ex);
+        }
+        return detail;
     }
 
     @Override
@@ -59,5 +76,12 @@ class MySqlDetailDao implements DetailDao {
         }
 
         return id;
+    }
+
+    private Detail fillDetail(ResultSet resultSet) throws SQLException{
+        Detail detail = new Detail();
+        detail.setId(resultSet.getLong(COLUMN_NAME_ID));
+        detail.setName(resultSet.getString(COLUMN_NAME_NAME));
+        return detail;
     }
 }
